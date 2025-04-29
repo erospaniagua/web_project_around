d05c9834e852d47b13eec2eb42b3ef571b47c60e
Eros <eros.gear@gmail.com>
2025-04-28 23:14:26 -0700

    final commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,19 @@
 //imports
 import { initialCards, card } from "./cards.js";
 import  {config, FormValidator } from "./validate.js";
+import {closePopup, ProfileEdit, OpenImgPop, closeImgPop, saveImg, closefromdiv} from "./utils.js"
 //botton editar perfil
 const popcontainer = document.querySelector("#popupProfile");
-
 const container= document.querySelector(".main");
-
 const editprofile = container.querySelector(".main__button_edit");
-
-
-
 editprofile.addEventListener('click', ()=>{
   popcontainer.classList.remove('popup_opened')
 });
 
-//boton cerrar pupup
+//boton cerrar popup
+closePopup();
 
-const closepopup = popcontainer.querySelector('.popup__close')
 
-closepopup.addEventListener('click', ()=>{
-  event.preventDefault();
-  popcontainer.classList.add('popup_opened')
-});
-
-//cerar popups haciendo click fuera de del popup
-
-const popcontainerdivs = document.querySelectorAll(".popup");
-const closefromdiv = (popupdiv)=>{popupdiv.addEventListener('click', ()=>{
-  if (event.target === event.currentTarget){
-    event.preventDefault();
-    popupdiv.classList.add('popup_opened');
-  }
-  });
-  
-}
-popcontainerdivs.forEach(closefromdiv);
 
 //poner el contenido del perfil en los campos del popup por defecto
 
@@ -51,18 +30,8 @@ popupname.placeholder = profilename.textContent;
 
 //Cambiar el contenido de la secion perfil con los inputs del pupup al guardar
 
-const popupProfile = popcontainer.querySelector('#newProfile');
 
-popupProfile.addEventListener('click', ()=>{
-  event.preventDefault();
-  profilename.textContent = popupname.value || profilename.textContent;
-  profilejob.textContent = popupjob.value || profilejob.textContent;
-  popcontainer.classList.add('popup_opened');
-  popupname.value = '';
-  popupjob.value = '';
-
-});
-
+ProfileEdit(container);
 
 //toma un array y crea las targetas usando un template
 
@@ -72,89 +41,29 @@ initialCards.forEach(element =>{
   cardRendering.createCard()
 
 })
-//
+// abre popup para agregar targetas
 
 
+OpenImgPop(container);
 
- // abre popup para agregar targetas
-
- const popupPlace = document.querySelector('#popupPlace');
-
- const addPlace = container.querySelector('.main__button_add')
-
- addPlace.addEventListener("click" , ()=>{
-  popupPlace.classList.remove('popup_opened')
- })
+ 
 
  //cierra el popup de para agregar lugares
-
- const closePlacepopup = popupPlace.querySelector('#popupPlaceClose')
-
-
-closePlacepopup.addEventListener('click', ()=>{
-  event.preventDefault();
-  popupPlace.classList.add('popup_opened')
-});
+ closeImgPop();
+ 
 
  //agrega targetas usando el boton add
 
- const saveNewPlace = document.querySelector('#newCard');
- const newTitle = popupPlace.querySelector('#title');
- const newLink = popupPlace.querySelector('#imgLink');
- saveNewPlace.addEventListener('submit', (event)=>{
-  event.preventDefault();
-  const cardAdded = new card(newTitle.value ,newLink.value, '#card')
-  cardAdded.createCard();
-  popupPlace.classList.add('popup_opened');
-  newTitle.value = '';
-  newLink.value = '';
-  const popcontainerdivs = document.querySelectorAll(".popup");
-  popcontainerdivs.forEach(closefromdiv);
+ saveImg();
 
- });
-
-//borra las targetas con el botton de trash
-/*const cardsContainer = document.querySelector('.gallery');
- cardsContainer.addEventListener("click", (event) => {
-  if (event.target.classList.contains("gallery__trash")) {
-    const cardElement = event.target.closest(".gallery__card");
-    cardElement.remove();
-  }
-});*/
-
-//agrega interactividad al botton de like on click
-
-/*cardsContainer.addEventListener("click", (event) => {
-  if (event.target.classList.contains("gallery__card-like")) {
-  const toggleLike = event.target.closest(".gallery__card-text");
-   toggleLike.querySelector(".gallery__card-like").classList.toggle("gallery__card-liked");
-  }
-});*/
-
-
-//abre la imagen haciendo click en ella
-
-
-
-
-//cierra la imagen al dar click en el boton cerrar
-
-
-
-
-
-
-//habilitar validacion de formularios
+//crea las istancias de Validacion
 const formList = document.querySelectorAll(config.popForm);
 formList.forEach((form)=>{
 const enableValidation = new FormValidator
 enableValidation.enableValidation(config, form);
 })
 
-
-
-
-
-
-
+//cerar popups haciendo click fuera de del popup
+ const popcontainerdivs = document.querySelectorAll(".popup");
+ popcontainerdivs.forEach(closefromdiv);
 
